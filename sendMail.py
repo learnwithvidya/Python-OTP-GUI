c9f85168e152f157817fa33504d7808ef40f248a
Vidya Rao <72331343+learnwithvidya@users.noreply.github.com>
2021-01-31 18:09:46 +0530

    Add files via upload

---
+++ sendMail.py
@@ -0,0 +1,21 @@
+#https://thecleverprogrammer.com/2020/08/24/otp-verification-gui-with-python/
+import os,math
+import random,sys
+import smtplib
+mailid=sys.argv[1]
+digits="0123456789"
+OTP=""
+for i in range(6):
+    OTP+=digits[math.floor(random.random()*10)]
+msg='Your OTP Verification for app is '+OTP+' Note..  Please enter otp within 2 minutes and 3 attempts, otherwise it becomes invalid'
+file2=open("otp.txt","w")
+file2.write(OTP)
+file2.close()
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("your email id", "app password")   #App password creation--> https://youtu.be/ndxUgivCszE
+print(msg)
+s.sendmail('your email id',mailid,msg)
+
+os.system('python otpveripage.py')
